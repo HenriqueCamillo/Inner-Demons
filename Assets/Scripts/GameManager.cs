30e440f994d2d09ceeba279f9ccede6dba1988a8
HenriqueCamillo <henriquecamillo@usp.br>
2020-08-22 17:54:57 -0300

    Menus and timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [SerializeField] float crysisDuration;
+    private float timeRemaining;
+    [SerializeField] TextMeshProUGUI timer;
+    [SerializeField] GameObject pauseMenu;
+    public bool isPaused;
+
+    public float TimeRemaining
+    {
+        get => timeRemaining;
+        set
+        {
+            timeRemaining = Mathf.Clamp(value, 0f, crysisDuration);
+            timer.text = (timeRemaining/60).ToString() + ":" + (timeRemaining%60).ToString().PadLeft(2, '0');
+
+            if (timeRemaining == 0)
+                GameOver();
+        }
+    }
 
     void Awake()
     {
@@ -15,8 +34,30 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+
+        TimeRemaining -= Time.deltaTime;
+    }
+
+    public void Pause()
+    {
+        if (isPaused)
+            Time.timeScale = 1f;
+        else 
+            Time.timeScale = 0f;
+
+        isPaused = !isPaused;
+        pauseMenu.SetActive(isPaused);
+    }
+
     public void GameOver()
     {
+        
         Debug.Log("Game over");
         // SceneManager.LoadScene(0);
     }
