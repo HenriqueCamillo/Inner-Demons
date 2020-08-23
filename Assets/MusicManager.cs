e37d47a324c2556c0a9e0a882886632d0397c3a5
Abner Santos <abner.santos@usp.br>
2020-08-23 20:56:37 -0300

    Added player sounds

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioIntro, _audioLoop;
+    [SerializeField] AudioClip songIntro, songLoop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioIntro.clip = songIntro;
+        _audioLoop.clip = songLoop;
+        _audioIntro.Play();
+        _audioLoop.PlayDelayed(songIntro.length);
+    }
+}
