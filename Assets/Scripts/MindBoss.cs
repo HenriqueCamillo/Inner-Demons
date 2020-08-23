af0b47ef081d6324f4dc4b27e68e99929bc8f692
HenriqueCamillo <henriquecamillo@usp.br>
2020-08-23 17:20:58 -0300

    Renatão 1.0

--- Assets/Scripts/MindBoss.cs
+++ Assets/Scripts/MindBoss.cs
@@ -134,18 +134,18 @@ public class MindBoss : Boss
         Debug.Log("Next attack: " + nextAttack);
         switch(nextAttack)
         {
-            // case Attack.GroundStomp:
-            //     Invoke(nameof(StartStomps), wait);
-            //     break;
-            // case Attack.TelegraphedHits:
-            //     Invoke(nameof(StartTelegraphed), wait);
-            //     break;
-            // case Attack.PropWaves:
-            //     Invoke(nameof(StartPropWaves), wait);
-            //     break;
-            // case Attack.TentacleFrenzy:
-            //     Invoke(nameof(StartTentacleFrenzy), wait);
-            //     break;
+            case Attack.GroundStomp:
+                Invoke(nameof(StartStomps), wait);
+                break;
+            case Attack.TelegraphedHits:
+                Invoke(nameof(StartTelegraphed), wait);
+                break;
+            case Attack.PropWaves:
+                Invoke(nameof(StartPropWaves), wait);
+                break;
+            case Attack.TentacleFrenzy:
+                Invoke(nameof(StartTentacleFrenzy), wait);
+                break;
             default:
                 Invoke(nameof(StartTentacleFrenzy), wait);
                 break;
